1ce49b477917a57ed064d8dbf0c04b0f947fd99c
Mariana Falcão <marianadealbfalcao@gmail.com>
2020-04-02 22:04:43 -0300

    iniciando header

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Trafico.</title>
+
+    <link rel="stylesheet" type="text/css" href="style/style.css"/>
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="img/icone.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/icone.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/icone.png">
+
+    <!-- JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"
+            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+</head>
+<body>
+<header class="header">
+    <nav class="nav">
+        <div class="nav-logo">
+            <img src="img/logo.png" alt="Trafico.">
+        </div>
+        <div class="nav-menu nav-menu-desktop">
+            <div class="nav-menu-item">
+                <a href="#sobre">
+                    About
+                </a>
+            </div>
+            <div class="nav-menu-item">
+                <a href="">
+                    Bow to
+                </a>
+            </div>
+            <div class="nav-menu-item">
+                <a href="#duvidas">
+                    FAQS
+                </a>
+            </div>
+            <div class="nav-menu-item">
+                <a href="#" class="botao-primario">
+                    Contact Us
+                </a>
+            </div>
+        </div>
+        <div class="nav-menu nav-menu-mobile">
+            <div class="nav-menu-item"></div>
+            <div class="nav-menu-item"></div>
+            <div class="nav-menu-item"></div>
+            <div class="nav-menu-item"></div>
+        </div>
+    </nav>
+    <h1>Your awesome<br>traffic permit<br>consultant.</h1>
+    <button class="botao-primario botao-icone">
+        Get Started
+        <?php include 'svg/arrow-right.php'; ?>
+    </button>
+</header>
+<section></section>
+</body>
+</html>
